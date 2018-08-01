0850eac53ac863c43825cf18ee3a96235886c513
Artur Quirino <arturquirino6@gmail.com>
2018-08-01 20:55:44 -0300

    Initial commit

---
+++ src/interfaces/ModelsInterface.ts
@@ -0,0 +1,10 @@
+import { CommentModel } from "../models/CommentModel";
+import { PostModel } from "../models/PostModel";
+import { UserModel } from "../models/UserModel";
+
+export interface ModelsInterface {
+    
+    Post: PostModel;
+    User: UserModel;
+    Comment: CommentModel;
+}
